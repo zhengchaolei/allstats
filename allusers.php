4da3393849290111ddfd7f30341db75798916166
eimmermann <eimmermann@06cb993e-4540-11de-9b56-810ca157e297>
2009-05-20 20:40:18 +0000

    Removed display of blank users

--- allusers.php
+++ allusers.php
@@ -298,7 +298,7 @@ $sql = "SELECT COUNT(a.id) as totgames, AVG(kills) as kills, AVG(deaths) as deat
 AVG(creepkills) as creepkills, AVG(creepdenies) as creepdenies,  AVG(neutralkills) as neutralkills, AVG(towerkills) as towerkills, 
 AVG(raxkills) as raxkills, AVG(courierkills) as courierkills, name 
 FROM dotaplayers AS a LEFT JOIN gameplayers AS b ON b.gameid = a.gameid and a.colour = b.colour 
-LEFT JOIN dotagames AS c ON c.gameid = a.gameid LEFT JOIN games as d ON d.id = c.gameid where c.winner <> 0";
+LEFT JOIN dotagames AS c ON c.gameid = a.gameid LEFT JOIN games as d ON d.id = c.gameid where c.winner <> 0 and name <> ''";
 if($ignorePubs)
 {
 $sql = $sql." and d.gamestate = '17'";
