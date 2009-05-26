df541dff50d53d68b83e97ffe8b9a80263d6f8de
eimmermann <eimmermann@06cb993e-4540-11de-9b56-810ca157e297>
2009-05-26 20:50:41 +0000

    Fixed SQL to work with SQLite

--- trunk/SQLSetup.sql
+++ trunk/SQLSetup.sql
@@ -1,122 +1,115 @@
-CREATE TABLE IF NOT EXISTS `originals` (
-  `heroid` varchar(4) NOT NULL,
-  `original` varchar(4) NOT NULL,
-  `description` varchar(32) NOT NULL,
-  PRIMARY KEY (`heroid`)
-) 
+CREATE TABLE IF NOT EXISTS originals (
+  heroid varchar(4) NOT NULL,
+  original varchar(4) NOT NULL,
+  description varchar(32) NOT NULL,
+  PRIMARY KEY (heroid)
+); 
 
---
--- Dataark for tabell `originals`
---
-
-INSERT INTO `originals` (`heroid`, `original`, `description`) VALUES
-('E002', 'orig', 'Lightning Revenant'),
-('E004', 'orig', 'Bone Fletcher'),
-('E005', 'orig', 'Moon Rider'),
-('E00P', 'orig', 'Twin Head Dragon'),
-('E015', 'U008', 'Lycanthrope'),
-('E01A', 'orig', 'Witch Doctor'),
-('E01B', 'orig', 'Spectre'),
-('E01C', 'orig', 'Warlock'),
-('E01Y', 'orig', 'Templar Assassin'),
-('EC45', 'orig', 'Faceless Void'),
-('EC57', 'orig', 'Venomancer'),
-('EC77', 'orig', 'Netherdrake'),
-('Edem', 'orig', 'Anti-Mage'),
-('Eevi', 'orig', 'Soul Keeper'),
-('Ekee', 'orig', 'Tormented Soul'),
-('Emns', 'orig', 'Prophet'),
-('Emoo', 'orig', 'Enchantress'),
-('Ewar', 'orig', 'Phantom Assassin'),
-('H000', 'orig', 'Centaur Warchief'),
-('H001', 'orig', 'Rogue Knight'),
-('H004', 'orig', 'Slayer'),
-('H008', 'orig', 'Bristleback'),
-('H00A', 'orig', 'Holy Knight'),
-('H00D', 'orig', 'Beastmaster'),
-('H00E', 'Hlgr', 'Dragon Knight'),
-('H00G', 'Hlgr', 'Dragon Knight'),
-('H00H', 'orig', 'Oblivion'),
-('H00I', 'orig', 'Geomancer'),
-('H00K', 'orig', 'Goblin Techies'),
-('H00N', 'orig', 'Dark Seer'),
-('H00Q', 'orig', 'Sacred Warrior'),
-('H00R', 'orig', 'Undying'),
-('H00S', 'orig', 'Storm Spirit'),
-('H00T', 'orig', 'Clockwerk Goblin'),
-('H00U', 'orig', 'Invoker'),
-('H00V', 'orig', 'Gorgon'),
-('H06S', 'orig', 'Admiral Proudmoore'),
-('H07I', 'H00R', 'Undying'),
-('Hamg', 'orig', 'Treant Protector'),
-('Harf', 'orig', 'Omniknight'),
-('Hblm', 'orig', 'Keeper of the Light'),
-('HC49', 'orig', 'Naga Siren'),
-('HC92', 'orig', 'Stealth Assassin'),
-('Hjai', 'orig', 'Crystal Maiden'),
-('Hlgr', 'orig', 'Dragon Knight'),
-('Hmbr', 'orig', 'Lord of Olympia'),
-('Hmkg', 'orig', 'Ogre Magi'),
-('Huth', 'orig', 'Ursa Warrior'),
-('Hvsh', 'orig', 'Bloodseeker'),
-('Hvwd', 'orig', 'Vengeful Spirit'),
-('N00B', 'orig', 'Faerie Dragon'),
-('N00R', 'orig', 'Pit Lord'),
-('N014', 'N01O', 'Lone Druid'),
-('N015', 'N01O', 'Lone Druid'),
-('N016', 'orig', 'Troll Warlord'),
-('N01A', 'orig', 'Silencer'),
-('N01I', 'orig', 'Alchemist'),
-('N01J', 'N01I', 'Alchemist'),
-('N01O', 'orig', 'Lone Druid'),
-('N01V', 'orig', 'Priestess of the Moon'),
-('N01W', 'orig', 'Shadow Priest'),
-('N02B', 'N016', 'Troll Warlord'),
-('N0EG', 'orig', 'Windrunner'),
-('Naka', 'orig', 'Bounty Hunter'),
-('Nbbc', 'orig', 'Juggernaut'),
-('Nbrn', 'orig', 'Drow Ranger'),
-('NC00', 'orig', 'Skeleton King'),
-('Nfir', 'orig', 'Shadow Fiend'),
-('Npbm', 'orig', 'Pandaren Brewmaster'),
-('Ntin', 'orig', 'Tinker'),
-('O00J', 'orig', 'Spiritbreaker'),
-('O00P', 'orig', 'Morphling'),
-('Ofar', 'orig', 'Tidehunter'),
-('Ogrh', 'orig', 'Phantom Lancer'),
-('Opgh', 'orig', 'Axe'),
-('Orkn', 'orig', 'Shadow Shaman'),
-('Oshd', 'orig', 'Bane Elemental'),
-('Otch', 'orig', 'Earthshaker'),
-('tes2', 'orig', 'test2beskrivelse'),
-('test', 'orig', 'testbeskrivelse'),
-('U000', 'orig', 'Nerubian Assassin'),
-('U006', 'orig', 'Broodmother'),
-('U008', 'orig', 'Lycanthrope'),
-('U00A', 'orig', 'Chaos Knight'),
-('U00C', 'orig', 'Lifestealer'),
-('U00E', 'orig', 'Necrolyte'),
-('U00F', 'orig', 'Butcher'),
-('U00K', 'orig', 'Sand King'),
-('U00P', 'orig', 'Obsidian Destroyer'),
-('Ubal', 'orig', 'Nerubian Weaver'),
-('UC01', 'orig', 'Queen of Pain'),
-('UC11', 'orig', 'Magnataur'),
-('UC18', 'orig', 'Demon Witch'),
-('UC42', 'orig', 'Doom Bringer'),
-('UC60', 'orig', 'Necro''lic'),
-('UC76', 'orig', 'Death Prophet'),
-('UC91', 'orig', 'Slithereen Guard'),
-('Ucrl', 'orig', 'Stone Giant'),
-('Udea', 'orig', 'Lord of Avernus'),
-('Udre', 'orig', 'Night Stalker'),
-('Uktl', 'orig', 'Enigma'),
-('Ulic', 'orig', 'Lich'),
-('Usyl', 'orig', 'Dwarven Sniper');
+INSERT INTO originals (heroid, original, description) VALUES ('E002', 'orig', 'Lightning Revenant');
+INSERT INTO originals (heroid, original, description) VALUES ('E004', 'orig', 'Bone Fletcher');
+INSERT INTO originals (heroid, original, description) VALUES ('E005', 'orig', 'Moon Rider');
+INSERT INTO originals (heroid, original, description) VALUES ('E00P', 'orig', 'Twin Head Dragon');
+INSERT INTO originals (heroid, original, description) VALUES ('E015', 'U008', 'Lycanthrope');
+INSERT INTO originals (heroid, original, description) VALUES ('E01A', 'orig', 'Witch Doctor');
+INSERT INTO originals (heroid, original, description) VALUES ('E01B', 'orig', 'Spectre');
+INSERT INTO originals (heroid, original, description) VALUES ('E01C', 'orig', 'Warlock');
+INSERT INTO originals (heroid, original, description) VALUES ('E01Y', 'orig', 'Templar Assassin');
+INSERT INTO originals (heroid, original, description) VALUES ('EC45', 'orig', 'Faceless Void');
+INSERT INTO originals (heroid, original, description) VALUES ('EC57', 'orig', 'Venomancer');
+INSERT INTO originals (heroid, original, description) VALUES ('EC77', 'orig', 'Netherdrake');
+INSERT INTO originals (heroid, original, description) VALUES ('Edem', 'orig', 'Anti-Mage');
+INSERT INTO originals (heroid, original, description) VALUES ('Eevi', 'orig', 'Soul Keeper');
+INSERT INTO originals (heroid, original, description) VALUES ('Ekee', 'orig', 'Tormented Soul');
+INSERT INTO originals (heroid, original, description) VALUES ('Emns', 'orig', 'Prophet');
+INSERT INTO originals (heroid, original, description) VALUES ('Emoo', 'orig', 'Enchantress');
+INSERT INTO originals (heroid, original, description) VALUES ('Ewar', 'orig', 'Phantom Assassin');
+INSERT INTO originals (heroid, original, description) VALUES ('H000', 'orig', 'Centaur Warchief');
+INSERT INTO originals (heroid, original, description) VALUES ('H001', 'orig', 'Rogue Knight');
+INSERT INTO originals (heroid, original, description) VALUES ('H004', 'orig', 'Slayer');
+INSERT INTO originals (heroid, original, description) VALUES ('H008', 'orig', 'Bristleback');
+INSERT INTO originals (heroid, original, description) VALUES ('H00A', 'orig', 'Holy Knight');
+INSERT INTO originals (heroid, original, description) VALUES ('H00D', 'orig', 'Beastmaster');
+INSERT INTO originals (heroid, original, description) VALUES ('H00E', 'Hlgr', 'Dragon Knight');
+INSERT INTO originals (heroid, original, description) VALUES ('H00G', 'Hlgr', 'Dragon Knight');
+INSERT INTO originals (heroid, original, description) VALUES ('H00H', 'orig', 'Oblivion');
+INSERT INTO originals (heroid, original, description) VALUES ('H00I', 'orig', 'Geomancer');
+INSERT INTO originals (heroid, original, description) VALUES ('H00K', 'orig', 'Goblin Techies');
+INSERT INTO originals (heroid, original, description) VALUES ('H00N', 'orig', 'Dark Seer');
+INSERT INTO originals (heroid, original, description) VALUES ('H00Q', 'orig', 'Sacred Warrior');
+INSERT INTO originals (heroid, original, description) VALUES ('H00R', 'orig', 'Undying');
+INSERT INTO originals (heroid, original, description) VALUES ('H00S', 'orig', 'Storm Spirit');
+INSERT INTO originals (heroid, original, description) VALUES ('H00T', 'orig', 'Clockwerk Goblin');
+INSERT INTO originals (heroid, original, description) VALUES ('H00U', 'orig', 'Invoker');
+INSERT INTO originals (heroid, original, description) VALUES ('H00V', 'orig', 'Gorgon');
+INSERT INTO originals (heroid, original, description) VALUES ('H06S', 'orig', 'Admiral Proudmoore');
+INSERT INTO originals (heroid, original, description) VALUES ('H07I', 'H00R', 'Undying');
+INSERT INTO originals (heroid, original, description) VALUES ('Hamg', 'orig', 'Treant Protector');
+INSERT INTO originals (heroid, original, description) VALUES ('Harf', 'orig', 'Omniknight');
+INSERT INTO originals (heroid, original, description) VALUES ('Hblm', 'orig', 'Keeper of the Light');
+INSERT INTO originals (heroid, original, description) VALUES ('HC49', 'orig', 'Naga Siren');
+INSERT INTO originals (heroid, original, description) VALUES ('HC92', 'orig', 'Stealth Assassin');
+INSERT INTO originals (heroid, original, description) VALUES ('Hjai', 'orig', 'Crystal Maiden');
+INSERT INTO originals (heroid, original, description) VALUES ('Hlgr', 'orig', 'Dragon Knight');
+INSERT INTO originals (heroid, original, description) VALUES ('Hmbr', 'orig', 'Lord of Olympia');
+INSERT INTO originals (heroid, original, description) VALUES ('Hmkg', 'orig', 'Ogre Magi');
+INSERT INTO originals (heroid, original, description) VALUES ('Huth', 'orig', 'Ursa Warrior');
+INSERT INTO originals (heroid, original, description) VALUES ('Hvsh', 'orig', 'Bloodseeker');
+INSERT INTO originals (heroid, original, description) VALUES ('Hvwd', 'orig', 'Vengeful Spirit');
+INSERT INTO originals (heroid, original, description) VALUES ('N00B', 'orig', 'Faerie Dragon');
+INSERT INTO originals (heroid, original, description) VALUES ('N00R', 'orig', 'Pit Lord');
+INSERT INTO originals (heroid, original, description) VALUES ('N014', 'N01O', 'Lone Druid');
+INSERT INTO originals (heroid, original, description) VALUES ('N015', 'N01O', 'Lone Druid');
+INSERT INTO originals (heroid, original, description) VALUES ('N016', 'orig', 'Troll Warlord');
+INSERT INTO originals (heroid, original, description) VALUES ('N01A', 'orig', 'Silencer');
+INSERT INTO originals (heroid, original, description) VALUES ('N01I', 'orig', 'Alchemist');
+INSERT INTO originals (heroid, original, description) VALUES ('N01J', 'N01I', 'Alchemist');
+INSERT INTO originals (heroid, original, description) VALUES ('N01O', 'orig', 'Lone Druid');
+INSERT INTO originals (heroid, original, description) VALUES ('N01V', 'orig', 'Priestess of the Moon');
+INSERT INTO originals (heroid, original, description) VALUES ('N01W', 'orig', 'Shadow Priest');
+INSERT INTO originals (heroid, original, description) VALUES ('N02B', 'N016', 'Troll Warlord');
+INSERT INTO originals (heroid, original, description) VALUES ('N0EG', 'orig', 'Windrunner');
+INSERT INTO originals (heroid, original, description) VALUES ('Naka', 'orig', 'Bounty Hunter');
+INSERT INTO originals (heroid, original, description) VALUES ('Nbbc', 'orig', 'Juggernaut');
+INSERT INTO originals (heroid, original, description) VALUES ('Nbrn', 'orig', 'Drow Ranger');
+INSERT INTO originals (heroid, original, description) VALUES ('NC00', 'orig', 'Skeleton King');
+INSERT INTO originals (heroid, original, description) VALUES ('Nfir', 'orig', 'Shadow Fiend');
+INSERT INTO originals (heroid, original, description) VALUES ('Npbm', 'orig', 'Pandaren Brewmaster');
+INSERT INTO originals (heroid, original, description) VALUES ('Ntin', 'orig', 'Tinker');
+INSERT INTO originals (heroid, original, description) VALUES ('O00J', 'orig', 'Spiritbreaker');
+INSERT INTO originals (heroid, original, description) VALUES ('O00P', 'orig', 'Morphling');
+INSERT INTO originals (heroid, original, description) VALUES ('Ofar', 'orig', 'Tidehunter');
+INSERT INTO originals (heroid, original, description) VALUES ('Ogrh', 'orig', 'Phantom Lancer');
+INSERT INTO originals (heroid, original, description) VALUES ('Opgh', 'orig', 'Axe');
+INSERT INTO originals (heroid, original, description) VALUES ('Orkn', 'orig', 'Shadow Shaman');
+INSERT INTO originals (heroid, original, description) VALUES ('Oshd', 'orig', 'Bane Elemental');
+INSERT INTO originals (heroid, original, description) VALUES ('Otch', 'orig', 'Earthshaker');
+INSERT INTO originals (heroid, original, description) VALUES ('U000', 'orig', 'Nerubian Assassin');
+INSERT INTO originals (heroid, original, description) VALUES ('U006', 'orig', 'Broodmother');
+INSERT INTO originals (heroid, original, description) VALUES ('U008', 'orig', 'Lycanthrope');
+INSERT INTO originals (heroid, original, description) VALUES ('U00A', 'orig', 'Chaos Knight');
+INSERT INTO originals (heroid, original, description) VALUES ('U00C', 'orig', 'Lifestealer');
+INSERT INTO originals (heroid, original, description) VALUES ('U00E', 'orig', 'Necrolyte');
+INSERT INTO originals (heroid, original, description) VALUES ('U00F', 'orig', 'Butcher');
+INSERT INTO originals (heroid, original, description) VALUES ('U00K', 'orig', 'Sand King');
+INSERT INTO originals (heroid, original, description) VALUES ('U00P', 'orig', 'Obsidian Destroyer');
+INSERT INTO originals (heroid, original, description) VALUES ('Ubal', 'orig', 'Nerubian Weaver');
+INSERT INTO originals (heroid, original, description) VALUES ('UC01', 'orig', 'Queen of Pain');
+INSERT INTO originals (heroid, original, description) VALUES ('UC11', 'orig', 'Magnataur');
+INSERT INTO originals (heroid, original, description) VALUES ('UC18', 'orig', 'Demon Witch');
+INSERT INTO originals (heroid, original, description) VALUES ('UC42', 'orig', 'Doom Bringer');
+INSERT INTO originals (heroid, original, description) VALUES ('UC60', 'orig', 'Necro''lic');
+INSERT INTO originals (heroid, original, description) VALUES ('UC76', 'orig', 'Death Prophet');
+INSERT INTO originals (heroid, original, description) VALUES ('UC91', 'orig', 'Slithereen Guard');
+INSERT INTO originals (heroid, original, description) VALUES ('Ucrl', 'orig', 'Stone Giant');
+INSERT INTO originals (heroid, original, description) VALUES ('Udea', 'orig', 'Lord of Avernus');
+INSERT INTO originals (heroid, original, description) VALUES ('Udre', 'orig', 'Night Stalker');
+INSERT INTO originals (heroid, original, description) VALUES ('Uktl', 'orig', 'Enigma');
+INSERT INTO originals (heroid, original, description) VALUES ('Ulic', 'orig', 'Lich');
+INSERT INTO originals (heroid, original, description) VALUES ('Usyl', 'orig', 'Dwarven Sniper');
 
 
 CREATE TABLE IF NOT EXISTS scores (
-	id INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , 
+	id INTEGER PRIMARY KEY  AUTOINCREMENT NOT NULL, 
 	category VARCHAR, 
 	name VARCHAR, 
 	server VARCHAR, 
